dea5f63608eaf6eaaa974812f32f0e9d60cf36b3
Hasitha Wikramasinghe <hasitha.softworxio@gmail.com>
2024-03-16 00:42:59 +0530

    Add project files.

---
+++ AspNetAuthenticationAuthorization/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AspNetAuthenticationAuthorization.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetAuthenticationAuthorization.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
